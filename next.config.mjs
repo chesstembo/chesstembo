104a54db1f364c74eb10baa86a4d1d2fdd25d3b8
chesstembo <chesstembo+github@gmail.com>
2026-01-24 18:23:36 +0300

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -2,33 +2,31 @@ import { withSentryConfig } from "@sentry/nextjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",      // Required for Capacitor (Static Build)
-  trailingSlash: false,  // false is generally better for Capacitor
+  output: "export",
+  trailingSlash: false,
   reactStrictMode: true,
   
-  // Required for Static Exports (Capacitor)
   images: {
     unoptimized: true,
   },
 
-  // Ignore strict checks to ensure build passes
   typescript: {
+    // Skips type checking during the build
     ignoreBuildErrors: true,
   },
+
   eslint: {
+    // Skips linting during the build
     ignoreDuringBuilds: true,
   },
   
-  // Optimize production code
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
 };
 
-// Export with Sentry wrapper
 export default withSentryConfig(nextConfig, {
-  // Sentry Webpack Plugin Options
   org: process.env.SENTRY_ORG,
-  project: "javascript-nextjs", // Ensure this matches your Sentry project name
-  silent: true, // Suppresses all logs
+  project: "javascript-nextjs",
+  silent: true,
 });
